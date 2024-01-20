4fda070699d44c7317087c97d58bc0da0cb4e981
Ryan Kendall <ryekendallion@gmail.com>
2024-01-19 20:40:04 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import sedanIcon from './images/icon-sedans.svg';
+import suvIcon from './images/icon-suvs.svg';
+import luxuryIcon from './images/icon-luxury.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div id="backdrop">
+        <div id="card">
+          <div className="info" id="sedan-info">
+            <div className="top-image">
+              <img src={sedanIcon} alt="sedan" className="carIcon"></img>
+            </div>
+            <h2 className="title">SEDANS</h2>
+            <p className="description">
+              Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.
+            </p>
+            <button className="learnMore" id="sedan-button">Learn More</button>
+          </div>
+          <div className="info" id="suv-info">
+            <div className="top-image">
+              <img src={suvIcon} alt="suv" className="carIcon"></img>
+            </div>
+            <h2 className="title">SUVS</h2>
+            <p className="description">
+              Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.
+            </p>
+            <button className="learnMore" id="suv-button">Learn More</button>
+          </div>
+          <div className="info" id="luxury-info">
+            <div className="top-image">
+              <img src={luxuryIcon} alt="luxury" className="carIcon"></img>
+            </div>
+            <h2 className="title">LUXURY</h2>
+            <p className="description">
+              Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.
+            </p>
+            <button className="learnMore" id="luxury-button">Learn More</button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
